72a736f7ac3b701febc0c681b41f207604b27fbd
Flix <139482310+Flix-Scripts@users.noreply.github.com>
2024-06-16 19:16:02 +0300

    Update cl_main.lua

--- flix-weedrun/cl_main.lua
+++ flix-weedrun/cl_main.lua
@@ -1,5 +1,5 @@
 -- Buyer spawn etc.
-function Randomsijainti()
+function Randomize()
     lib.notify({
         title = Config.Language['notifytitle'],
         description = Config.Language['deliverdropoff'],
@@ -8,23 +8,23 @@ function Randomsijainti()
 
     local waypoint =  Config.Dropoffpoints[math.random(#Config.Dropoffpoints)]
 
-    local model = Randompedi[math.random(1, #Randompedi)]
+    local model = Randomped[math.random(1, #Randomped)]
     RequestModel(model)
     while not HasModelLoaded(model) do
         Wait(1000)
     end
 
-    local kauppias = CreatePed(4, model, waypoint.coords, true, false)
-    FreezeEntityPosition(kauppias, true)
-    SetEntityInvincible(kauppias, true)
-    TaskStartScenarioInPlace(kauppias, 'WORLD_HUMAN_HANG_OUT_STREET', true)
-    KannabisBlip = AddBlipForCoord(waypoint.coords)
-    SetBlipSprite (KannabisBlip, Config.Blip['sprite'])
-    SetBlipScale  (KannabisBlip, 1.2)
-    SetBlipColour(KannabisBlip, Config.Blip['colour'])
+    local buyer = CreatePed(4, model, waypoint.coords, true, false)
+    FreezeEntityPosition(buyer, true)
+    SetEntityInvincible(buyer, true)
+    TaskStartScenarioInPlace(buyer, 'WORLD_HUMAN_HANG_OUT_STREET', true)
+    CannabisBlip = AddBlipForCoord(waypoint.coords)
+    SetBlipSprite (CannabisBlip, Config.Blip['sprite'])
+    SetBlipScale  (CannabisBlip, 1.2)
+    SetBlipColour(CannabisBlip, Config.Blip['colour'])
     BeginTextCommandSetBlipName("STRING")
     AddTextComponentSubstringPlayerName('Dropoff')
-    EndTextCommandSetBlipName(KannabisBlip)
+    EndTextCommandSetBlipName(CannabisBlip)
 
 -- Policealert
 local function PoliceAlert()
@@ -34,11 +34,11 @@ local function PoliceAlert()
 end
 
 -- Animation
-    local function Animaatio()
-        TaskTurnPedToFaceEntity(kauppias, PlayerPedId(), 1.0)
-        TaskTurnPedToFaceEntity(PlayerPedId(), kauppias, 1.0)
+    local function Animation()
+        TaskTurnPedToFaceEntity(buyer, PlayerPedId(), 1.0)
+        TaskTurnPedToFaceEntity(PlayerPedId(), buyer, 1.0)
         Wait(100)
-        PlayAmbientSpeech1(kauppias, "Generic_Hi", "Speech_Params_Force")
+        PlayAmbientSpeech1(buyer, "Generic_Hi", "Speech_Params_Force")
         Wait(1000)
         PoliceAlert()
 
@@ -47,85 +47,85 @@ end
         TaskPlayAnim(PlayerPedId(), "mp_safehouselost@", "package_dropoff", 8.0, 1.0, -1, 16, 0, 0, 0, 0)
         Wait(1000)
 
-        PlayAmbientSpeech1(kauppias, "Chat_State", "Speech_Params_Force")
+        PlayAmbientSpeech1(buyer, "Chat_State", "Speech_Params_Force")
         Wait(500)
         RequestAnimDict("mp_safehouselost@")
         while not HasAnimDictLoaded("mp_safehouselost@") do Wait(10) end
-        TaskPlayAnim(kauppias, "mp_safehouselost@", "package_dropoff", 8.0, 1.0, -1, 16, 0, 0, 0, 0 )
+        TaskPlayAnim(buyer, "mp_safehouselost@", "package_dropoff", 8.0, 1.0, -1, 16, 0, 0, 0, 0 )
         Wait(1500)
     end
 
 -- Functions for buyertarget
-local function OstoTarget()
+local function BuyerTarget()
     local count = exports.ox_inventory:Search('count', 'cannabis')
         if count > Config.Cannabis['amountneeded'] then
         exports.ox_target:disableTargeting(true)
-        exports.ox_target:removeLocalEntity(kauppias, 'weedrunostaja')
-        exports.ox_target:removeLocalEntity(kauppias, 'weedrunostajaa')
-        Animaatio()
+        exports.ox_target:removeLocalEntity(buyer, 'weedrunbuyer')
+        exports.ox_target:removeLocalEntity(buyer, 'weedrunbuyera')
+        Animation()
         exports.ox_target:disableTargeting(false)
-        lib.callback.await('huumekaupat:reward', source)
-        RemoveBlip(KannabisBlip)
-        FreezeEntityPosition(kauppias, false)
-        SetEntityInvincible(kauppias, false)
-        SetBlockingOfNonTemporaryEvents(kauppias, false)
-	    SetPedAsNoLongerNeeded(kauppias)
+        lib.callback.await('weedrun:reward', source)
+        RemoveBlip(CannabisBlip)
+        FreezeEntityPosition(buyer, false)
+        SetEntityInvincible(buyer, false)
+        SetBlockingOfNonTemporaryEvents(buyer, false)
+	    SetPedAsNoLongerNeeded(buyer)
         lib.notify({
             title = Config.Language['notifytitle'],
             description = Config.Language['deliverdropoff2'],
             type = 'success'
         })
         Wait(15000)
-        DeletePed(kauppias)
+        DeletePed(buyer)
         Wait(20000)
-        Randomsijainti()
+        Randomize()
         else
         lib.notify({
         title = Config.Language['notifytitle'],
         description = Config.Language['notenoughcannabis'],
         type = 'error'
         })
-        RemoveBlip(KannabisBlip)
-        exports.ox_target:removeLocalEntity(kauppias, 'weedrunostaja')
-        exports.ox_target:removeLocalEntity(kauppias, 'weedrunostajaa')
-        FreezeEntityPosition(kauppias, false)
-        SetEntityInvincible(kauppias, false)
-	    SetPedAsNoLongerNeeded(kauppias)
+        RemoveBlip(CannabisBlip)
+        exports.ox_target:removeLocalEntity(buyer, 'weedrunbuyer')
+        exports.ox_target:removeLocalEntity(buyer, 'weedrunbuyera')
+        FreezeEntityPosition(buyer, false)
+        SetEntityInvincible(buyer, false)
+	    SetPedAsNoLongerNeeded(buyer)
         Wait(15000)
-        DeletePed(kauppias)
+        DeletePed(buyer)
     end
 end
 
-local function OstoTarget2()
+local function BuyerTarget2()
     exports.ox_target:disableTargeting(true)
     Wait(100)
-    exports.ox_target:removeLocalEntity(kauppias, 'weedrunostaja')
-    exports.ox_target:removeLocalEntity(kauppias, 'weedrunostajaa')
+    exports.ox_target:removeLocalEntity(buyer, 'weedrunbuyer')
+    exports.ox_target:removeLocalEntity(buyer, 'weedrunbuyera')
     Wait(100)
     exports.ox_target:disableTargeting(false)
-    RemoveBlip(KannabisBlip)
-    SetPedAsNoLongerNeeded(kauppias)
+    RemoveBlip(CannabisBlip)
+    SetPedAsNoLongerNeeded(buyer)
     Wait(15000)
-    DeletePed(kauppias)
+    DeletePed(buyer)
 end
 -- Buyer
-    exports.ox_target:addLocalEntity(kauppias, {
+    exports.ox_target:addLocalEntity(buyer, {
         {
-            name = 'weedrunostaja',
+            name = 'weedrunbuyer',
             label = Config.Language['buyerlabel'],
             icon = 'fa-solid fa-cannabis',
             distance = 2,
             onSelect = function()
-                OstoTarget()
+                BuyerTarget()
             end
         },
         {
-            name = 'weedrunostajaa',
+            name = 'weedrunbuyera',
             label = Config.Language['stoprun'],
             icon = 'fa-solid fa-x',
             distance = 2,
             onSelect = function()
-                OstoTarget2()
+                BuyerTarget2()
             end
         }})
     end
@@ -137,7 +137,7 @@ Dropoffpoints = {
     {coords = vec4(-1046.91, 500.32, 83.16, 232.91)},
 }
 
-Randompedi = {
+Randomped = {
     0x68709618,
     0x62018559,
     0xCF623A2C,
@@ -156,14 +156,14 @@ CreateThread(function()
         Wait(1000)
     end
 
-    local weedrunpedi = CreatePed(4, model, Config.StartLocation.x, Config.StartLocation.y, Config.StartLocation.z-1.0, Config.StartLocation.w, false, false)
-    FreezeEntityPosition(weedrunpedi, true)
-    SetEntityInvincible(weedrunpedi, true)
-    SetBlockingOfNonTemporaryEvents(weedrunpedi, true)
-    TaskStartScenarioInPlace(weedrunpedi, 'WORLD_HUMAN_SMOKING', true)
-    exports.ox_target:addLocalEntity(weedrunpedi, {
+    local startnpc = CreatePed(4, model, Config.StartLocation.x, Config.StartLocation.y, Config.StartLocation.z-1.0, Config.StartLocation.w, false, false)
+    FreezeEntityPosition(startnpc, true)
+    SetEntityInvincible(startnpc, true)
+    SetBlockingOfNonTemporaryEvents(startnpc, true)
+    TaskStartScenarioInPlace(startnpc, 'WORLD_HUMAN_SMOKING', true)
+    exports.ox_target:addLocalEntity(startnpc, {
         {
-            name = 'weedrunpedi',
+            name = 'startnpc',
             label = Config.Language['startrun'],
             icon = 'fa-regular fa-comments',
             distance = 2,
@@ -174,7 +174,7 @@ CreateThread(function()
                     type = 'info'
                 })
                 Wait(30000)
-                Randomsijainti()
+                Randomize()
             end
         }
     })
